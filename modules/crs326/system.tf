1436c31b4fca48c3714f115070334f5f2a265e4f
Mircea-Pavel ANTON <contact@mirceanton.com>
2025-01-08 15:27:54 +0000

    set timezone

--- modules/crs326/system.tf
+++ modules/crs326/system.tf
@@ -5,3 +5,13 @@
 resource "routeros_system_identity" "identity" {
   name = "Rack Slow"
 }
+
+
+# =================================================================================================
+# System Timezone
+# https://registry.terraform.io/providers/terraform-routeros/routeros/latest/docs/resources/system_clock
+# =================================================================================================
+resource "routeros_system_clock" "timezone" {
+  time_zone_name       = "Europe/Bucharest"
+  time_zone_autodetect = false
+}
