f76c8ac025cf4278fd073e63f78f4e70d07c2c48
Mircea-Pavel ANTON <contact@mirceanton.com>
2025-01-08 15:23:07 +0000

    configure system identity

---
+++ modules/rb5009/system.tf
@@ -0,0 +1,7 @@
+# =================================================================================================
+# System Identity
+# https://registry.terraform.io/providers/terraform-routeros/routeros/latest/docs/resources/system_identity
+# =================================================================================================
+resource "routeros_system_identity" "identity" {
+  name = "Router"
+}
