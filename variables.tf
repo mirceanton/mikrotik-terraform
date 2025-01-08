e17eff28a7fae7ba98978b52587c95d8965d5e3a
Mircea-Pavel ANTON <contact@mirceanton.com>
2025-01-08 15:33:06 +0000

    update variables

--- variables.tf
+++ variables.tf
@@ -11,3 +11,27 @@ variable "mikrotik_password" {
   description = "The password to authenticate against the RouterOS API."
   sensitive   = true
 }
+
+
+## ================================================================================================
+## PPPoE Connection Variables
+## ================================================================================================
+variable "digi_pppoe_password" {
+  type        = string
+  sensitive   = true
+  description = "The PPPoE password for the Digi connection."
+}
+variable "digi_pppoe_username" {
+  type        = string
+  sensitive   = true
+  description = "The PPPoE username for the Digi connection."
+}
+
+## ================================================================================================
+## WiFi Variables
+## ================================================================================================
+variable "wifi_password" {
+  type        = string
+  sensitive   = true
+  description = "The password for the Wi-Fi network."
+}
