75973c347b9779e98d87b86ba09e099abe0e83d1
Mircea-Pavel ANTON <contact@mirceanton.com>
2025-02-28 00:04:51 +0200

    implement module and remove unused files

--- code/main.tf
+++ code/main.tf
@@ -1,3 +1,44 @@
+locals {
+  timezone       = "Europe/Bucharest"
+  cloudflare_ntp = "time.cloudflare.com"
+}
+
+module "rb5009_mig" {
+  source = "./modules/base"
+  mikrotik_ip       = "10.0.0.1"
+  mikrotik_username = var.mikrotik_username
+  mikrotik_password = var.mikrotik_password
+  mikrotik_insecure = true
+
+  hostname    = "Router"
+  timezone    = local.timezone
+  ntp_servers = [local.cloudflare_ntp]
+}
+
+module "crs326_mig" {
+  source = "./modules/base"
+  mikrotik_ip       = "10.0.0.3"
+  mikrotik_username = var.mikrotik_username
+  mikrotik_password = var.mikrotik_password
+  mikrotik_insecure = true
+
+  hostname    = "Rach Slow"
+  timezone    = local.timezone
+  ntp_servers = [local.cloudflare_ntp]
+}
+
+module "hex_mig" {
+  source = "./modules/base"
+  mikrotik_ip       = "10.0.0.4"
+  mikrotik_username = var.mikrotik_username
+  mikrotik_password = var.mikrotik_password
+  mikrotik_insecure = true
+
+  hostname    = "Living Room Switch"
+  timezone    = local.timezone
+  ntp_servers = [local.cloudflare_ntp]
+}
+
 module "rb5009" {
   source                  = "./devices/rb5009"
   mikrotik_host_url       = "https://10.0.0.1"
@@ -9,14 +50,6 @@ module "rb5009" {
   untrusted_wifi_password = var.untrusted_wifi_password
   guest_wifi_password     = var.guest_wifi_password
   iot_wifi_password       = var.iot_wifi_password
-}
-
-module "crs317" {
-  source            = "./devices/crs317"
-  mikrotik_host_url = "https://10.0.0.2"
-  mikrotik_username = var.mikrotik_username
-  mikrotik_password = var.mikrotik_password
-  mikrotik_insecure = true
 }
 
 module "crs326" {
