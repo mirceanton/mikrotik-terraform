fa1f8dfb13f552d1b307c59f8d5710599fe728b3
Mircea-Pavel Anton <contact@mirceanton.com>
2025-05-14 00:27:02 +0300

    feat(rb5009): add wireguard fw rules

--- router-firewall-rules.tf
+++ router-firewall-rules.tf
@@ -89,8 +89,83 @@ resource "routeros_ip_firewall_filter" "accept_router_established_related_untrac
   action           = "accept"
   chain            = "input"
   connection_state = "established,related,untracked"
-  place_before     = routeros_ip_firewall_filter.accept_trusted_input.id
+  place_before     = routeros_ip_firewall_filter.allow_wireguard_connections.id
 }
+
+# WIREGUARD
+resource "routeros_ip_firewall_filter" "allow_wireguard_connections" {
+  provider     = routeros.rb5009
+  comment      = "Allow Wireguard Incoming Connections"
+  action       = "accept"
+  chain        = "input"
+  protocol     = "udp"
+  dst_port     = routeros_interface_wireguard.wireguard.listen_port
+  place_before = routeros_ip_firewall_filter.allow_wireguard_to_internet.id
+}
+
+resource "routeros_ip_firewall_filter" "allow_wireguard_to_internet" {
+  provider           = routeros.rb5009
+  comment            = "Allow Wireguard to Internet"
+  action             = "accept"
+  chain              = "forward"
+  in_interface       = routeros_interface_wireguard.wireguard.name
+  out_interface_list = routeros_interface_list.wan.name
+  place_before       = routeros_ip_firewall_filter.allow_wireguard_to_untrusted.id
+}
+resource "routeros_ip_firewall_filter" "allow_wireguard_to_untrusted" {
+  provider      = routeros.rb5009
+  comment       = "Allow Wireguard to Untrusted"
+  action        = "accept"
+  chain         = "forward"
+  in_interface  = routeros_interface_wireguard.wireguard.name
+  out_interface = local.vlans.Untrusted.name
+  place_before  = routeros_ip_firewall_filter.allow_wireguard_to_k8s.id
+}
+resource "routeros_ip_firewall_filter" "allow_wireguard_to_k8s" {
+  provider         = routeros.rb5009
+  comment          = "Allow Wireguard to K8S Services"
+  action           = "accept"
+  chain            = "forward"
+  in_interface     = routeros_interface_wireguard.wireguard.name
+  out_interface    = local.vlans.Kubernetes.name
+  dst_address_list = routeros_ip_firewall_addr_list.k8s_services.list
+  place_before     = routeros_ip_firewall_filter.drop_wireguard_forward.id
+}
+resource "routeros_ip_firewall_filter" "drop_wireguard_forward" {
+  provider     = routeros.rb5009
+  comment      = "Drop all Wireguard forward"
+  action       = "drop"
+  chain        = "forward"
+  in_interface = routeros_interface_wireguard.wireguard.name
+  place_before = routeros_ip_firewall_filter.allow_wireguard_dns_tcp.id
+}
+resource "routeros_ip_firewall_filter" "allow_wireguard_dns_tcp" {
+  provider     = routeros.rb5009
+  comment      = "Allow local DNS (TCP) for Wireguard"
+  action       = "accept"
+  chain        = "input"
+  protocol     = "tcp"
+  in_interface = routeros_interface_wireguard.wireguard.name
+  place_before = routeros_ip_firewall_filter.allow_wireguard_dns_udp.id
+}
+resource "routeros_ip_firewall_filter" "allow_wireguard_dns_udp" {
+  provider     = routeros.rb5009
+  comment      = "Allow local DNS (UDP) for Wireguard"
+  action       = "accept"
+  chain        = "input"
+  protocol     = "udp"
+  in_interface = routeros_interface_wireguard.wireguard.name
+  place_before = routeros_ip_firewall_filter.drop_wireguard_input.id
+}
+resource "routeros_ip_firewall_filter" "drop_wireguard_input" {
+  provider     = routeros.rb5009
+  comment      = "Drop all Wireguard input"
+  action       = "drop"
+  chain        = "input"
+  in_interface = routeros_interface_wireguard.wireguard.name
+  place_before = routeros_ip_firewall_filter.accept_trusted_input.id
+}
+
 
 # TRUSTED
 resource "routeros_ip_firewall_filter" "accept_trusted_input" {
