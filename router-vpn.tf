8027f08cb430c2c2600c10001572d99d0d83aa63
Mircea-Pavel Anton <contact@mirceanton.com>
2025-05-14 00:50:05 +0300

    style: terraform fmt

--- router-vpn.tf
+++ router-vpn.tf
@@ -1,6 +1,6 @@
 # DDNS
 resource "routeros_ip_cloud" "cloud" {
-  provider = routeros.rb5009
+  provider             = routeros.rb5009
   ddns_enabled         = "yes"
   update_time          = true
   ddns_update_interval = "1m"
@@ -25,7 +25,7 @@ resource "cloudflare_dns_record" "example_dns_record" {
 # https://registry.terraform.io/providers/terraform-routeros/routeros/latest/docs/resources/interface_wireguard
 # =================================================================================================
 resource "routeros_interface_wireguard" "wireguard" {
-  provider = routeros.rb5009
+  provider    = routeros.rb5009
   name        = "wg0"
   comment     = "Wireguard VPN"
   listen_port = "13231"
@@ -37,7 +37,7 @@ resource "routeros_interface_wireguard" "wireguard" {
 # https://registry.terraform.io/providers/terraform-routeros/routeros/latest/docs/resources/ip_address
 # =================================================================================================
 resource "routeros_ip_address" "wireguard" {
-  provider = routeros.rb5009
+  provider  = routeros.rb5009
   address   = "192.168.255.1/24"
   interface = routeros_interface_wireguard.wireguard.name
   comment   = "Wireguard VPN"
