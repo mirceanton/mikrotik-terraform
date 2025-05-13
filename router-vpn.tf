cf191f567b8e27f982e798d60259ba3b6d4b86f1
Mircea-Pavel Anton <contact@mirceanton.com>
2025-05-13 22:55:22 +0300

    feat(rb5009): create wireguard interface + IP address

--- router-vpn.tf
+++ router-vpn.tf
@@ -19,3 +19,27 @@ resource "cloudflare_dns_record" "example_dns_record" {
   ttl     = 3600
   content = routeros_ip_cloud.cloud.dns_name
 }
+
+# =================================================================================================
+# Wireguard Interface
+# https://registry.terraform.io/providers/terraform-routeros/routeros/latest/docs/resources/interface_wireguard
+# =================================================================================================
+resource "routeros_interface_wireguard" "wireguard" {
+  provider = routeros.rb5009
+  name        = "wg0"
+  comment     = "Wireguard VPN"
+  listen_port = "13231"
+  mtu         = 1420
+}
+
+# =================================================================================================
+# Wireguard IP Address
+# https://registry.terraform.io/providers/terraform-routeros/routeros/latest/docs/resources/ip_address
+# =================================================================================================
+resource "routeros_ip_address" "wireguard" {
+  provider = routeros.rb5009
+  address   = "192.168.255.1/24"
+  interface = routeros_interface_wireguard.wireguard.name
+  comment   = "Wireguard VPN"
+  network   = "192.168.255.0"
+}
