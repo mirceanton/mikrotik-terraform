2e55881363ecb84223eca66bfd19b365bf13fbb0
Mircea-Pavel Anton <contact@mirceanton.com>
2025-12-17 00:55:15 +0200

    fix: remove ddns hostname output

--- infrastructure/mikrotik/router-services/_outputs.tf
+++ infrastructure/mikrotik/router-services/_outputs.tf
@@ -1,9 +1,3 @@
-output "ddns_hostname" {
-  description = "Mikrotik Cloud DDNS hostname"
-  value       = routeros_ip_cloud.cloud.dns_name
-  sensitive   = true
-}
-
 output "untrusted_wifi_password" {
   description = "Password for the untrusted WiFi network"
   value       = random_string.untrusted_wifi_password.result
